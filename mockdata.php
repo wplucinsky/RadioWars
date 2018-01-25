a41b6e0891300c40c50cad96d548ad7555e17c58
wplucinsky <wplucinsky@gmail.com>
2018-01-25 13:21:35 -0500

    update mock data script to accept POST requests

--- mockdata.php
+++ mockdata.php
@@ -1,4 +1,6 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+
 	$data = array();
 
 	function getRandomNode($min, $max, $parentNode = null){
@@ -56,7 +58,12 @@
 		$packetsSent['node'.getRandomNode(0,20,$p)] = rand(1,10);
 		$packetsSent['node'.getRandomNode(0,20,$p)] = rand(1,10);
 
-		$packetsReceived['node'.($p-13)] = ($p-13)*$m;
+		if ( $m == 1 ){
+			$packetsReceived['node'.($p-13)] = ($p-13)*20;
+		} else {
+			$packetsReceived['node'.($p-13)] = ($p-13)*$m;
+		}
+		
 		$packetsReceived['node'.($p-7)]  = ($p-7)*$m;
 
 
@@ -64,6 +71,35 @@
 		$data['packetsReceived'] = $packetsReceived;
 
 		return $data;
+	}
+
+	function postData($data){
+		$radio = array(
+			'rxGain' => rand(5,20).'.0',
+			'txGain' => rand(5,20).'.0',
+			'normalFrequency' => rand(1,9).'00e6',
+			'power' => rand(1,9).'00',
+			'sampleRate' => rand(1,9).'0e3',
+			'frameSize' => strval(rand(1,16)*128),
+			'nodeId' => '15',
+			'usrpAddress' => '192.168.11.'.rand(5,20),
+			'nodeAddress' => '10.10.10'.rand(5,20),
+			'radioDirection' => rand(1,2) == 1 ? 'Left' : 'Right',
+			'captured' => '0',
+			'_id' => '15'
+		);
+
+		foreach ($data as $key => $prop) {
+			if ( array_key_exists($key, $radio)) {
+				if ($key == '_id' || $key == 'nodeId') {
+					$radio['_id'] = $prop['value'];
+					$radio['nodeId'] = $prop['value'];
+				} else {
+					$radio[$key] = $prop['value'];
+				}
+			}
+		}
+		return $radio;
 	}
 
 	if ( isset($_GET['build']) && $_GET['build'] == 1) {
@@ -77,11 +113,12 @@
 		for ($i=0; $i < $c; $i++) { 
 			$data[$i] = getBuildingData($i, $m);
 		}
+	} elseif ( isset($_GET['post'])) {
+		$data = postData($_POST);
 	} else {
 		// random data
 		$data[0] = getRandomData();
 		$data[1] = getRandomData();
 	}
 
-	header('Access-Control-Allow-Origin: *');
 	echo json_encode($data);
